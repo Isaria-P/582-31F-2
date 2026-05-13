e792e0687011274092e124021520c088db029c38
DESKTOP-8KEFPBJ\Owner <isaria1234@gmail.com>
2026-05-13 00:27:56 -0400

    homework002 with errors fixed & bootstrap for styling.

--- homework002/script.js
+++ homework002/script.js
@@ -1,23 +1,37 @@
 const loadUser = document.getElementById("load-user")
-const statusMessage = document.getElementById("status")
+const userCard = document.getElementById("user-card")
+const title = document.getElementById("title")
+const status = document.getElementById("status")
+const p = document.getElementById("discription")
 // console.log(statusBTN)
 
+let userData = [];
 
 // 2. Fetch one user
 fetch("https://jsonplaceholder.typicode.com/users/1")
-
     .then((response) => {
-        // console.log(response)
         return response.json()
     })
     .then((user) => {
-        // const parseuser = JSON.Parse(user)
-        console.log(typeof user)
-        loadUser.addEventListener("click", () => {
-            statusMessage.textContent =  JSON.stringify(user, null, 2)
-        })
-        return users
+        console.log(user.name)
+        userData = user;        
+        return JSON.stringify(user, null, 2)
     })
     .catch((error) => {
-        statusMessage.textContent = "User not found"
+        status.textContent = "User not found"
     })
+
+
+loadUser.addEventListener("click", () => {
+    // userCard.textContent =  JSON.stringify(userData, null, 2)
+    userCard.textContent = `
+        Name: ${userData.name}, 
+        Email: ${userData.email},
+        Phone: ${userData.Phone},
+        City: ${userData.city}`
+    
+    title.textContent = userData.name
+    status.textContent = "Successfully Loaded"
+    p.textContent = userData.username
+});
+    
