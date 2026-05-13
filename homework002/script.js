5baaa7bf8145013ff02b6aae07660ecc65ccdad0
DESKTOP-8KEFPBJ\Owner <isaria1234@gmail.com>
2026-05-12 23:17:07 -0400

    add 1st commit, errors need to be fixed.

---
+++ homework002/script.js
@@ -0,0 +1,24 @@
+const loadUser = document.getElementById("load-user")
+const statusMessage = document.getElementById("status")
+// console.log(statusBTN)
+
+
+// 2. Fetch one user
+fetch("https://jsonplaceholder.typicode.com/users/1")
+
+    .then((response) => {
+        // console.log(response)
+        return response.json()
+    })
+    .then((user) => {
+        // const parseuser = JSON.Parse(user)
+        console.log(typeof user)
+        loadUser.addEventListener("click", () => {
+            statusMessage.textContent =  JSON.stringify(user, null, 2)
+            
+        })
+        return users
+    })
+    .catch((error) => {
+        statusMessage.textContent = "User not found"
+    })
