3c3788910c632308bf7d13f491d039d828af8f5c
DESKTOP-8KEFPBJ\Owner <isaria1234@gmail.com>
2026-05-14 00:40:17 -0400

    Exercise006 select the Dom.

---
+++ exercise006/script.js
@@ -0,0 +1,7 @@
+const loadPostbtn = document.getElementById("load-post");
+const statusP = document.getElementById("status");
+const postContent = document.getElementById("post-content");
+
+// console.log(loadPostbtn, statusP, postBody)
+
+
