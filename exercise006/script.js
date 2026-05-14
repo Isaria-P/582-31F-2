7fa1bdf83654948f5e5d8966979fcad6aaa7ea7d
DESKTOP-8KEFPBJ\Owner <isaria1234@gmail.com>
2026-05-14 00:41:54 -0400

    Exercise006 fetch data and add event listeners.

--- exercise006/script.js
+++ exercise006/script.js
@@ -4,4 +4,28 @@ const postContent = document.getElementById("post-content");
 
 // console.log(loadPostbtn, statusP, postBody)
 
+// addEventListener for btn
+loadPostbtn.addEventListener("click", () => {
+    statusP.textContent = "Loading...."
+    
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+        .then((response) => {
+        if (!response) {
+            // console.log(response)
+            throw new Error(`HTTP error: ${response.status}`)
+        }
+        return response.json();
+    })
+    .then((post) => {
+        // postTitle.textContent = 
+        postContent.innerHTML = `
+            <h2>Profile Information</h2>
+            <p>Title: ${post.title}</p>
+            <p>Email: ${post.body}</p>
+    
+        `;
+        console.log(post)
+        statusP.textContent = "User loaded successfully"
+    })
 
+})
