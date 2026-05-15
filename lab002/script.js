1f0751c2a4365cdac600c5a6e5b614dcdaa42509
DESKTOP-8KEFPBJ\Owner <isaria1234@gmail.com>
2026-05-14 20:35:44 -0400

    Display Loading Message inside cards.

--- lab002/script.js
+++ lab002/script.js
@@ -1,10 +1,44 @@
 const loadUserBtn = document.getElementById("load-user-btn")
 const statusArea = document.getElementById("status")
+const clearBtn = document.getElementById("clear-button");
+const cardContainer = document.getElementById("cardContainer")
+const cards = document.querySelectorAll("div.cards")
 
-// console.log(loadUserBtn, statusArea)
+console.log(cards, statusArea)
+// Users https://jsonplaceholder.typicode.com/users
 
+// Posts https://jsonplaceholder.typicode.com/posts
 
 loadUserBtn.addEventListener("click", () => {
-    statusArea.textContent = ""
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => {
+            // console.log(response)
+            if (!response.ok) {
+                throw new Error(`HTTP error: ${response.status}`);
+            }
+            const parseUsers = response.json()
+            // console.log(`ParsedUser: ${parseUsers}`)
+            return parseUsers
+        })
+        .then((users) => {
+            const firstFiveUders = users.slice(0,5)
+            // console.log(firstFiveUders)
+
+            // Loading message
+            cards.forEach(card => {
+                card.innerHTML = `
+                    <p>loading Cards....</p>
+                `
+                return card
+            })
+            .then((user))
+            
+        })
+
 })
 
+
+clearBtn.addEventListener("click", () => {
+    statusArea.textContent = ""
+})
