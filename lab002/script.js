f17fc7dc3d8568c3da265166dab4451b72c31b63
DESKTOP-8KEFPBJ\Owner <isaria1234@gmail.com>
2026-05-16 22:38:46 -0400

    tying to fix render the first 3 posts but im not happy with my results.

--- lab002/script.js
+++ lab002/script.js
@@ -67,10 +67,10 @@ function renderUserCard(user) {
 
     const loadPostBtn = card.querySelector(".load-post-btn");
     const postsContainer = card.querySelector(".posts");
-    
-    loadUserBtn.addEventListener("click", () => { 
-        loadPostForUser(user, postsContainer);
+    loadPostBtn.addEventListener("click", () => { 
+        loadPostsForUser(user, postsContainer);
     });
+
     return card;
 }
 
@@ -79,8 +79,10 @@ function loadPostsForUser(user, postsContainer) {
     setSatus(`Loading Post for ${user.name}....`, "loading");
 
     /* First 3 post only */
-    const firstThreePost = allPosts.slice(0, 3);
-    renderUserCard(firstThreePost, postsContainer);
+    const firstThreePost = allPosts
+        .filter((post) => post.userId === user.id)
+        .slice(0, 3);
+    renderPosts(firstThreePost, postsContainer);
 
     setSatus(`Posts loaded for ${user.name}`, "success");
 }
@@ -96,118 +98,9 @@ function renderPosts(posts, container) {
         `;
     }).join("");
     container.innerHTML = postHTML;
-
 }
 
 // Button Events
 loadUserBtn.addEventListener("click", loadUsers);
 clearBtn.addEventListener("click", clearDashBoard)
-
-// loadUserBtn.addEventListener("click", () => {
-    // status.innerHTML = `<p>loading Cards....</p>`
-    
-      
- 
-
-        // .then(([users, posts]) => {
-            
-        //     allPosts = posts;
-        //     cards.innerHTML = "";
-        //     const firstFiveUsers = users.slice(0,5)
-
-        //     // const cardsHTML =
-        //     firstFiveUsers.map((user) => {
-        //         const userCard = renderUserCard(user);
-        //         cards.appendChild(userCard);
-        //     });
-
-            // return `
-            //     <div class="cards">
-            // //         <h5>${user.name}</h5>
-            // //         <p>Email: ${user.email}</p>
-            // //         <p>Phone: ${user.phone}</p>
-            // //         <p>City: ${user.address.city}</p>
-            // //         <p>Company: ${user.company.name}</p>
-            // //         <br>
-            // //         <button class="load-post-btn" data-userid="${user.id}">
-            // //             Load Posts
-            // //         </button>
-            // //         <div class="posts" id="posts-${user.id}"></div>
-            // //     </div>
-            // // `;
-        // });
-        // cards.innerHTML = cardsHTML.join("");        // console.log(cardsHTML)
-    // })
-    // .catch((error) => {
-    //     cards.innerHTML = `<p>Failed to load</p>`
-    // });
-// })
-
-// function loadPosts(userId) {
-    
-//     //const userPost = allPosts.slice(0, 3);
-//         // .filter((post) => post.userId === userId)
-        
-//     // const postHTML = userPost.map((post) => {
-//     //     return `
-//     //     <div class="post">
-//     //         <p>${post.title}</p>
-//     //         <p>${post.body}</p>
-//     //         </div>
-//     //     `;
-
-//     // }).join("")
-
-//     document.getElementById(`posts-${userId}`).innerHTML = postHTML;
-// }
-
-// // button click 
-// cards.addEventListener("click", (e) => {
-//     if (e.target.classList.contains("load-post-btn")) {
-//         const userId = Number(e.target.dataset.userid);
-//         loadPosts(userId)
-//     }
-// });
-
-
-
-
-
-// clearBtn.addEventListener("click", () => {
-//     status.textContent = ""
-// })
-
-
-
-// function 
-
-
-
-
-// const firstThreePost = userPost.slice(0,3)
-            // console.log(firstThreePost)
-
-            // const postHTML = firstThreePost.map((post) => {
-            //     return `
-            //         <div class="post">
-
-            //         <h3>${post.title}</h3>
-
-            //         <p>${post.body}</p>
-
-            //     </div>
-            //     `;
-            // }).join("")
-
-            // cards
-
-            // cards.innerHTML = `
-            //     <h2>Name: ${user.name}</h2>
-            //     <p>Email: ${user.email}</p>
-            //     <p>Email: ${user.phone}</p>
-            //     <p>Email: ${user.address.city}</p>
-            //     <p>Email: ${user.company.name}</p>
-            // `;
-
-                // ${postHTML}
 
