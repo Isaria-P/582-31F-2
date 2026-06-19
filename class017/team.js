cfdcb45d8f2cd89ddd962c36f7694ce483100533
DESKTOP-8KEFPBJ\Owner <isaria1234@gmail.com>
2026-06-19 17:04:36 -0400

    creating the Team class.

--- class017/team.js
+++ class017/team.js
@@ -12,5 +12,30 @@ export class Team {
         return `${this.name} - ${this.group} - ${this._points}`
     }
 
-    
+    set points(value) {
+        if (value < 0) {
+            throw new Error("Points can not be of a negative Value.")
+        }
+        this._points
+    }
+
+    get points() {
+        return this._points
+    }
+
+    static fromObject(data) {
+        return new Team(
+            data.id,
+            data.name,
+            data.group,
+            data.points,
+            data.played,
+            data.goalDifference,
+        )
+    }
 }
+
+const Vincy = new Team(909, "St.Vincent", "Group V", 2, "Mexico", 2);
+console.log(Vincy.group)
+Vincy.points = 6;
+console.log(Vincy)
