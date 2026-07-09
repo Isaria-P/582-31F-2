8395e863d8d017ab920c5d7f8aefbca5f7ca291f
DESKTOP-8KEFPBJ\Owner <isaria1234@gmail.com>
2026-07-09 19:42:24 -0400

    Added the Artist class.

---
+++ class019/artist.js
@@ -0,0 +1,39 @@
+export class Artist {
+    constructor(id, name, genre, stage, time, country, headliner) {
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+        this.stage = stage;
+        this.time = time;
+        this.country = country;
+        this. headliner = headliner;
+    }
+
+    get summary() {
+        return `${this.name}-${this.genre}-${this.stage}`
+    }
+    set headliner(value){
+        if (typeof value !== "boolean") {
+            throw new Error("Headliner must be true or false")
+        } else {
+            this._headliner = value
+        }
+    }
+    get headliner() {
+        return this._headliner;
+    }
+    static fromObject(data) {
+        return new Artist(
+            data.id,
+            data.name,
+            data.genre,
+            data.stage,
+            data.time,
+            data.country,
+            data.headliner,
+        )
+    }
+}
+
+const bey = new Artist(444, "Beyonce", "RNB", "Left", "10pm", "France", true);
+console.log(bey.headliner)
